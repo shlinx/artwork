792e52c476df056bbcb05dbea305db700daf44a8
Shawn Lin <shawnxiaolin@gmail.com>
2017-06-17 15:53:50 +1200

    Add multiple led colors feature.

--- main.py
+++ main.py
@@ -9,6 +9,20 @@ AUDIOS = [
     'Waves & seagull.mp3'
 ]
 
+LED_PINS = [
+    17, 21, 22,
+]
+
+LED_COLORS = [
+    [17],
+    [21],
+    [22],
+    [17, 21],
+    [17, 22],
+    [21, 22],
+    [17, 21, 22]
+]
+
 sys.path.append(os.getcwd())
 
 try:
@@ -20,24 +34,18 @@ except ImportError:
 class Artwork:
     work_path = os.path.dirname(os.path.abspath(__file__))
 
-    def __init__(self, sensor_pin=4, led_pin=17):
+    def __init__(self, sensor_pin=4):
         self.pir = MotionSensor(sensor_pin)
-        self.led = LED(led_pin)
+        self.led_pins = []
         self.count = int(time.time())
         self.pir.when_motion = self.on_when_motion
         self.pir.when_no_motion = self.on_when_no_motion
         self.audio_file_index = 0
+        self.led_color_index = 0
+        for pin in LED_PINS:
+            self.led_pins.append(LED(pin))
         print('Artwork is watching motions...')
         pause()
-
-    def get_audio_file(self):
-        current = os.path.join(self.work_path, 'audios', AUDIOS[self.audio_file_index])
-        self.audio_file_index += 1
-        try:
-            AUDIOS[self.audio_file_index]
-        except IndexError:
-            self.audio_file_index = 0
-        return current
 
     def on_when_motion(self):
         self.count = int(time.time())
@@ -46,7 +54,6 @@ class Artwork:
         self.play_sound()
         self.control_led(on=False)
         print("Sleeping for 60 seconds")
-        print('\n')
         time.sleep(60)
 
     def on_when_no_motion(self):
@@ -54,14 +61,12 @@ class Artwork:
         gap = new_time - self.count
         self.count = new_time
         print('Motion lost, lasted {gap} seconds.'.format(gap=gap))
-        print('\n')
 
     def play_sound(self):
         current = os.path.join(self.work_path, 'audios', AUDIOS[self.audio_file_index])
         print("Playing {}".format(AUDIOS[self.audio_file_index]))
         os.system('mpg123 -q "{}"'.format(current))
         print("Finish Playing {}.".format(AUDIOS[self.audio_file_index]))
-        print('\n')
         self.audio_file_index += 1
         try:
             AUDIOS[self.audio_file_index]
@@ -69,11 +74,19 @@ class Artwork:
             self.audio_file_index = 0
 
     def control_led(self, on=True):
+        led_pins = self.led_pins[self.led_color_index]
         if on:
-            print("Turning on LED...")
-            self.led.on()
+            print("Turning on LED for pins {}".format(led_pins))
+            for pin in led_pins:
+                pin.on()
         else:
-            print("Turning off LED...")
-            self.led.off()
+            print("Turning off LED for pins {}".format(led_pins))
+            for pin in led_pins:
+                pin.off()
+            self.led_color_index += 1
+            try:
+                self.led_pins[self.led_color_index]
+            except IndexError:
+                self.led_color_index = 0
 
 Artwork()
